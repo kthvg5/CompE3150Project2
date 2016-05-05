e33641c986d2ed7564b9cff46bef370dd5536e12
kthvg5 <kthvg5@mst.edu>
2016-05-05 16:05:46 -0500

    map 1 for testing

---
+++ map1.c
@@ -0,0 +1,31 @@
+//This map will primarily be used for testing and will be the easy level
+/*
+
+map:
+WWWWWWW
+WWWWWWW
+WLLDWWW
+WWWDWWW
+WWWDWWW
+WWWGWWW
+WWWWWWW
+
+key:
+W: Wall, Represented by a solit light
+R: Right is the quickest way to goal, Represented by no light
+L: Left is the quickest way to goal, Represented by no light
+U: Up is the quickest way to goal, Represented by no light
+D: Down is the quickest way to goal, Represented by no light
+G: goal, Represented by a blinking light
+*/
+char map1[7][7] = {
+  {'W','W','W','W','W','W', 'W'} ,
+  {'W','W','W','W','W','W', 'W'} ,
+  {'W','L','L','D','W','W', 'W'} ,
+  {'W','W','W','D','W','W', 'W'} ,
+  {'W','W','W','D','W','W', 'W'} ,
+  {'W','W','W','G','W','W', 'W'} ,
+  {'W','W','W','W','W','W', 'W'} ,
+}
+int M1startX = 1;
+int M1startY = 2;
